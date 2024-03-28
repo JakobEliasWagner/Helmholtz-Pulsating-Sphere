30c44ca80b1b74df32a04fd4e61dbef401063265
Jakob <jakob@winter-wind.de>
2024-03-28 18:11:10 +0100

    add utility to write dataclasses to dict

---
+++ hps/utils/nested_dataclass_to_dict.py
@@ -0,0 +1,38 @@
+from dataclasses import (
+    fields,
+    is_dataclass,
+)
+import numpy as np
+from typing import (
+    Any,
+)
+import inspect
+from hps.functions import Function
+
+
+def dataclass_to_dict(obj: Any) -> Any:
+    """
+    Recursively converts a nested dataclass into a dictionary.
+
+    Args:
+        obj: The dataclass instance to convert.
+
+    Returns:
+        A dictionary representation of the dataclass, including nested dataclasses.
+    """
+    if is_dataclass(obj):
+        result = {}
+        for field in fields(obj):
+            value = getattr(obj, field.name)
+            result[field.name] = dataclass_to_dict(value)
+        return result
+    elif isinstance(obj, list):
+        return [dataclass_to_dict(item) for item in obj]
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, dict):
+        return {key: dataclass_to_dict(value) for key, value in obj.items()}
+    elif isinstance(obj, Function):
+        return inspect.getsource(obj.mapping)
+    else:
+        return obj
