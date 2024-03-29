d2a2ff7a6eed3d442a5ff2553ba498666c99d7e8
JakobEliasWagner <jakob@winter-wind.de>
2024-03-29 16:26:36 +0100

    add minor changes

--- hps/helmholtz.py
+++ hps/helmholtz.py
@@ -17,109 +17,88 @@ from dolfinx.fem.petsc import LinearProblem  # isort: skip
 class Helmholtz:
     def __init__(self, properties: RunProperties):
         self.properties = properties
+        self.progress = None
 
-    def __call__(self, out_dir: pathlib.Path):
-        # write run properties
-        description_file = out_dir.joinpath("properties.json")
-        with open(description_file, "w") as file_handle:
-            json.dump(dataclass_to_dict(self.properties), file_handle)
-
+    def solve_subset(
+        self,
+        indices: np.ndarray,
+        msh_path: pathlib.Path,
+        out_dir: pathlib.Path,
+        properties: RunProperties,
+    ):
         # mesh
-        msh_path = out_dir.joinpath("mesh.msh")
-        msh_builder = MeshBuilder(self.properties)
-        msh_builder(msh_path)
-
-        # get mesh into fenics
         msh, ct, ft = get_mesh(msh_path, comm=MPI.COMM_SELF)
+        ds = ufl.Measure("ds", msh, subdomain_data=ft)
+        tdim = msh.topology.dim
+        top_tol = properties.domain.box_lengths[1] / 10
+        top_cells = dolfinx.mesh.locate_entities(
+            msh, tdim, lambda x: x[1] + top_tol >= properties.domain.box_lengths[1]
+        )
+        right_tol = properties.domain.box_lengths[0] / 10
+        right_cells = dolfinx.mesh.locate_entities(
+            msh,
+            tdim,
+            lambda x: x[0] + right_tol >= properties.domain.box_lengths[0],
+        )
 
-        # function spaces
+        # variational problem
         v = dolfinx.fem.FunctionSpace(
             msh, ufl.FiniteElement("Lagrange", msh.ufl_cell(), 2)
         )
         v_plot = dolfinx.fem.FunctionSpace(
             msh, ufl.FiniteElement("Lagrange", msh.ufl_cell(), 1)
         )
-
-        # define domain parameters
-        v0 = 1e-3
-
-        ds = ufl.Measure("ds", msh, subdomain_data=ft)
-        tdim = msh.topology.dim
-        top_tol = self.properties.domain.box_lengths[1] / 10
-        top_cells = dolfinx.mesh.locate_entities(
-            msh, tdim, lambda x: x[1] + top_tol >= self.properties.domain.box_lengths[1]
-        )
-        right_tol = self.properties.domain.box_lengths[0] / 10
-        right_cells = dolfinx.mesh.locate_entities(
-            msh,
-            tdim,
-            lambda x: x[0] + right_tol >= self.properties.domain.box_lengths[0],
-        )
-
-        # Define variational problem
         p = ufl.TrialFunction(v)
         xi = ufl.TestFunction(v)
-
         p_sol = dolfinx.fem.Function(v)
         p_sol.name = "p"
-
         y_top = dolfinx.fem.Function(v)
         y_right = dolfinx.fem.Function(v)
 
-        # derived physics constants
-        s = 1j * self.properties.physics.rho * self.properties.physics.c
+        # physics constants
+        v0 = 1e-3
+        s = 1j * properties.physics.rho * properties.physics.c
 
-        # Start writer
-        filename = out_dir.joinpath("solution.xdmf")
+        # initialize out file and writer
+        out_file = out_dir.joinpath(f"solution_{min(indices)}.xdmf")
         writer = dolfinx.io.XDMFFile(
-            MPI.COMM_SELF, filename, "w", encoding=dolfinx.io.XDMFFile.Encoding.HDF5
+            MPI.COMM_SELF, out_file, "w", encoding=dolfinx.io.XDMFFile.Encoding.HDF5
         )
         writer.write_mesh(msh)
 
-        pbar = tqdm(total=self.properties.n_observations)
-        for i, (top_param, right_param, f) in enumerate(
-            zip(
-                self.properties.top_samples,
-                self.properties.right_samples,
-                self.properties.frequency_samples,
-            )
-        ):
-            # interpolate boundaries
-            y_top.interpolate(
-                lambda x: self.properties.top_boundary(top_param)(x), cells=top_cells
-            )
-            y_right.interpolate(
-                lambda x: self.properties.right_boundary(right_param)(x),
-                cells=right_cells,
-            )
+        # solve different systems
+        for idx in indices:
+            # get parameters
+            top_params = properties.top_samples[idx]
+            right_params = properties.right_samples[idx]
+            f = properties.frequency_samples[idx]
 
-            # setup physics
-            wave_length = self.properties.physics.c / f
+            # derive physics parameters
+            wave_length = properties.physics.c / f
             k = 2 * np.pi / wave_length
             ks = k**2
+            y_top.interpolate(
+                lambda x: properties.top_boundary(top_params)(x), cells=top_cells
+            )
+            y_right.interpolate(
+                lambda x: properties.right_boundary(right_params)(x),
+                cells=right_cells,
+            )
 
             # setup problem
             lhs = (
                 ufl.inner(ufl.grad(p), ufl.grad(xi)) * ufl.dx
                 - (ks * ufl.inner(p, xi) * ufl.dx)
-                - (
-                    s
-                    * k
-                    * y_top
-                    * ufl.inner(p, xi)
-                    * ds(self.properties.mesh.top_boundary)
-                )
+                - (s * k * y_top * ufl.inner(p, xi) * ds(properties.mesh.top_boundary))
                 - (
                     s
                     * k
                     * y_right
                     * ufl.inner(p, xi)
-                    * ds(self.properties.mesh.right_boundary)
+                    * ds(properties.mesh.right_boundary)
                 )
             )
-            rhs = (
-                s * k * ufl.inner(v0, xi) * ds(self.properties.mesh.excitation_boundary)
-            )
+            rhs = s * k * ufl.inner(v0, xi) * ds(properties.mesh.excitation_boundary)
             # compute solution
             problem = LinearProblem(
                 lhs,
@@ -136,9 +115,32 @@ class Helmholtz:
             # write solution
             out_function = dolfinx.fem.Function(v_plot)
             out_function.interpolate(p_sol)
-            writer.write_function(out_function, i)
+            writer.write_function(out_function, idx)
 
-            pbar.update()
+            self.progress.update()
 
         # Close writer
         writer.close()
+
+    def __call__(self, out_dir: pathlib.Path):
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+        # write run properties
+        description_file = out_dir.joinpath("properties.json")
+        with open(description_file, "w") as file_handle:
+            json.dump(dataclass_to_dict(self.properties), file_handle)
+
+        # mesh
+        msh_path = out_dir.joinpath("mesh.msh")
+        msh_builder = MeshBuilder(self.properties)
+        msh_builder(msh_path)
+
+        # solve
+        self.progress = tqdm(total=self.properties.n_observations)
+        self.solve_subset(
+            np.arange(self.properties.n_observations),
+            msh_path,
+            out_dir,
+            self.properties,
+        )
+        self.progress = None
--- hps/properties/run_properties.py
+++ hps/properties/run_properties.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 
 import numpy as np
+from scipy.spatial.distance import cdist
 
 from hps.functions import Function, UniformSampler
 
@@ -29,6 +30,38 @@ class RunProperties:
     right_samples: np.array = field(init=False)
 
     def __post_init__(self):
-        self.frequency_samples = self.frequency_sampler(n_samples=self.n_observations)
-        self.top_samples = self.top_sampler(n_samples=self.n_observations)
-        self.right_samples = self.right_sampler(n_samples=self.n_observations)
+        # initial samples
+        frequency_samples = self.frequency_sampler(n_samples=self.n_observations)
+        top_samples = self.top_sampler(n_samples=self.n_observations)
+        right_samples = self.right_sampler(n_samples=self.n_observations)
+
+        # preprocess initial (unscaled dimensionality leads to inf distance)
+        fs = (frequency_samples.reshape(-1, 1) - np.min(frequency_samples)) / (
+            np.max(frequency_samples) - np.min(frequency_samples)
+        )
+        ts = (top_samples - np.min(top_samples)) / (
+            np.max(top_samples) - np.min(top_samples)
+        )
+        rs = (right_samples - np.min(right_samples)) / (
+            np.max(right_samples) - np.min(right_samples)
+        )
+
+        # sort values to speedup computation
+        samples = np.concatenate([fs, ts, rs], axis=1)
+        # Calculate the pairwise distances between all rows
+        distances = cdist(samples, samples, metric="euclidean")
+        # Set large diagonal to avoid selecting the same point
+        np.fill_diagonal(distances, np.inf)
+        # Start with the first point, could be any point as a starting point
+        sorted_indices = [0]
+        while len(sorted_indices) < self.n_observations:
+            last_index = sorted_indices[-1]
+            # Find the closest point that hasn't been visited
+            closest_next_index = np.argmin(distances[last_index])
+            # Update the distances to ensure the same point is not chosen again
+            distances[:, last_index] = np.inf
+            sorted_indices.append(closest_next_index)
+
+        self.top_samples = top_samples[sorted_indices]
+        self.right_samples = right_samples[sorted_indices]
+        self.frequency_samples = frequency_samples[sorted_indices]
