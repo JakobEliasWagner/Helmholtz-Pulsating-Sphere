350a51f62de5ed1c6ec68209ce5fa2a294bea6fa
JakobEliasWagner <jakob@winter-wind.de>
2024-03-29 11:27:13 +0100

    fix for new run property

--- hps/helmholtz.py
+++ hps/helmholtz.py
@@ -65,11 +65,13 @@ class Helmholtz:
         )
         writer.write_mesh(msh)
 
-        pbar = tqdm(
-            total=len(self.properties.top_parameters) * len(self.properties.frequencies)
-        )
-        for i, (top_param, right_param) in enumerate(
-            zip(self.properties.top_parameters, self.properties.right_parameters)
+        pbar = tqdm(total=self.properties.n_observations)
+        for i, (top_param, right_param, f) in enumerate(
+            zip(
+                self.properties.top_samples,
+                self.properties.right_samples,
+                self.properties.frequency_samples,
+            )
         ):
             # interpolate boundaries
             y_top.interpolate(lambda x: self.properties.top_boundary(top_param)(x))
@@ -77,57 +79,49 @@ class Helmholtz:
                 lambda x: self.properties.right_boundary(right_param)(x)
             )
 
-            for f in self.properties.frequencies:
-                # setup physics
-                wave_length = self.properties.physics.c / f
-                k = 2 * np.pi / wave_length
-                ks = k**2
+            # setup physics
+            wave_length = self.properties.physics.c / f
+            k = 2 * np.pi / wave_length
+            ks = k**2
 
-                # setup problem
-                lhs = (
-                    ufl.inner(ufl.grad(p), ufl.grad(xi)) * ufl.dx
-                    - ks * ufl.inner(p, xi) * ufl.dx
-                    - s
-                    * k
-                    * y_top
-                    * ufl.inner(p, xi)
-                    * ds(self.properties.mesh.top_boundary)
-                    - s
-                    * k
-                    * y_right
-                    * ufl.inner(p, xi)
-                    * ds(self.properties.mesh.right_boundary)
-                )
-                rhs = (
-                    s
-                    * k
-                    * ufl.inner(v0, xi)
-                    * ds(self.properties.mesh.excitation_boundary)
-                )
+            # setup problem
+            lhs = (
+                ufl.inner(ufl.grad(p), ufl.grad(xi)) * ufl.dx
+                - ks * ufl.inner(p, xi) * ufl.dx
+                - s
+                * k
+                * y_top
+                * ufl.inner(p, xi)
+                * ds(self.properties.mesh.top_boundary)
+                - s
+                * k
+                * y_right
+                * ufl.inner(p, xi)
+                * ds(self.properties.mesh.right_boundary)
+            )
+            rhs = (
+                s * k * ufl.inner(v0, xi) * ds(self.properties.mesh.excitation_boundary)
+            )
 
-                # compute solution
-                problem = LinearProblem(
-                    lhs,
-                    rhs,
-                    u=p_sol,
-                    petsc_options={
-                        "ksp_type": "preonly",
-                        "pc_type": "cholesky",
-                        "pc_factor_mat_solver_type": "mumps",
-                    },
-                )
-                problem.solve()
+            # compute solution
+            problem = LinearProblem(
+                lhs,
+                rhs,
+                u=p_sol,
+                petsc_options={
+                    "ksp_type": "preonly",
+                    "pc_type": "cholesky",
+                    "pc_factor_mat_solver_type": "mumps",
+                },
+            )
+            problem.solve()
 
-                # abuse xdmf time to encode frequency and run id
-                biggest_frequency = max(self.properties.frequencies)
-                frequency_part = f / (10 ** (np.floor(np.log10(biggest_frequency)) + 1))
-                encoding = frequency_part + i
+            # write solution
+            out_function = dolfinx.fem.Function(v_plot)
+            out_function.interpolate(p_sol)
+            writer.write_function(out_function, i)
 
-                # write solution
-                out_function = dolfinx.fem.Function(v_plot)
-                out_function.interpolate(p_sol)
-                writer.write_function(out_function, encoding)
+            pbar.update()
 
-                pbar.update()
         # Close writer
         writer.close()
