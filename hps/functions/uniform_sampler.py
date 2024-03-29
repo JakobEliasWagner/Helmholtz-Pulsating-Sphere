a1c9ca1079715656f068b75f418811e425cd9b88
JakobEliasWagner <jakob@winter-wind.de>
2024-03-29 11:26:31 +0100

    add squeeze output

--- hps/functions/uniform_sampler.py
+++ hps/functions/uniform_sampler.py
@@ -6,4 +6,5 @@ from .sampler import Sampler
 class UniformSampler(Sampler):
     def __call__(self, n_samples: int) -> np.array:
         samples = np.random.rand(n_samples, self.ndim)
-        return samples * self.delta_x + self.min_x
+        samples = samples * self.delta_x + self.min_x
+        return samples.squeeze()
