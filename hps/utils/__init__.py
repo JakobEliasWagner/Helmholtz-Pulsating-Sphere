420bda2df158e6b45c4b1f26d17929abb5e625f5
JakobEliasWagner <jakob@winter-wind.de>
2024-04-30 21:29:05 +0200

    add file parsing

--- hps/utils/__init__.py
+++ hps/utils/__init__.py
@@ -1,4 +1,5 @@
 from .dataclass_to_dict import dataclass_to_dict
 from .unique_id import UniqueId
+from .xml_to_numpy import xdmf_to_numpy
 
-__all__ = ["UniqueId", "dataclass_to_dict"]
+__all__ = ["UniqueId", "dataclass_to_dict", "xdmf_to_numpy"]
